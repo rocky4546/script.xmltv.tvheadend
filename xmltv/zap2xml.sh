60cdd87ffcda11e0e1c1fd86c170042d07b6ab30
rocky4546 <rcmccaslin@hotmail.com>
2017-03-10 17:52:18 -0600

    Initial baseline

--- xmltv/zap2xml.sh
+++ xmltv/zap2xml.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+path=`dirname $0`
+logfile="${path}/../.kodi/temp/zap2xml.log"
+tempTvXmlFile="xmltv_default.xml"
+
+rm -f $logfile
+# create the default xmltv.xml
+${path}/zap2xml.pl -D -S 3  > ${logfile} 2>&1
+#${path}/zap2xml.pl -D > ${logfile} 2>&1
+
+# if the file was generated, then update it
+file=`find $path -name ${tempTvXmlFile} -mtime -1`
+if [[ -z "$file" ]] ; then
+  echo "ERROR:: ${tempTvXmlFile} file did not generate" >> ${logfile} 2>&1
+else
+  ${path}/category-filter.pl ${path}/${tempTvXmlFile} >${path}/xmltv.xml 2>>${logfile}
+  echo "SUCCESS:: New xmltv.xml file generated" >> ${logfile} 2>&1
+fi
 
