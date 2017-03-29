b028af75d591ea8afd5d0da1606286e98953be79
rocky4546 <rcmccaslin@hotmail.com>
2017-03-28 22:30:05 -0500

    Add files via upload

--- xmltv/zap2xml.sh
+++ xmltv/zap2xml.sh
@@ -3,6 +3,18 @@ path=`dirname $0`
 logfile="${path}/../.kodi/temp/zap2xml.log"
 tempTvXmlFile="xmltv_default.xml"
 
+if [[ "$1" == "fast" ]] ; then
+  shift
+  options="-D"
+  echo "Executing with no delays"
+  echo "This will put a load on zap2it.com"
+  echo "Please be careful when using this command"
+else
+  options="-D -S 3"
+fi
+
+options="$options $@"
+
 logpath=`dirname $logfile`
 if [[ ! -d $logpath ]] ; then
   # kodi may not be installed.  use folder where
@@ -12,8 +24,13 @@ fi
 
 rm -f $logfile
 # create the default xmltv.xml
-${path}/zap2xml.pl -D -S 3  > ${logfile} 2>&1
-#${path}/zap2xml.pl -D > ${logfile} 2>&1
+# Determine if the default xmltv.xml was created in the last 12 hours
+file=`find $path -name ${tempTvXmlFile} -mtime -1`
+if [[ ! -z "$file" ]] ; then
+  options="$options -N 0"
+fi
+${path}/zap2xml.pl $options  > ${logfile} 2>&1
+
 
 # if the file was generated, then update it
 file=`find $path -name ${tempTvXmlFile} -mtime -1`
